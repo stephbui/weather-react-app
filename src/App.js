75925b69f66f67e516ee2e02c399268ae0f08232
stephbui <stephanie.bui@umontreal.ca>
2021-01-29 16:27:22 -0500

    add the git hub repo url

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React from "react";
 import './App.css';
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return <div className="App">
+    <h1>Weather App</h1>
+    <footer>
+      This project was coded rby Stephanie Bui and is {""}
+     <a href="https://github.com/stephbui/weather-react-app" target="_blank">
+        open-sourced on GitHub</a>  
+    </footer>
+   
+  </div>;
+
 }
 export default App;
