54be179760bedd6a1e4aebbb2b1b481dccffb168
stephbui <stephanie.bui@umontreal.ca>
2021-01-30 20:37:05 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import './App.css';
 
-function App() {
-  return <div className="App">
-    <h1>Weather App</h1>
-    <footer>
-      This project was coded rby Stephanie Bui and is {""}
-     <a href="https://github.com/stephbui/weather-react-app" target="_blank">
-        open-sourced on GitHub</a>  
-    </footer>
-   
-  </div>;
-
+export default function App() {
+  return (
+  <div className="App">
+    <div className="container">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Stephanie Bui and is {""}
+        <a href="https://github.com/stephbui/weather-react-app" target="_blank"> 
+          open-sourced on GitHub 
+        </a>  
+      </footer>
+    </div>
+  </div>
+  );
 }
-export default App;
