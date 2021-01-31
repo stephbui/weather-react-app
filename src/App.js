c141ce285d3c9cf9b5471b8472b6110c2cd9c8d8
stephbui <stephanie.bui@umontreal.ca>
2021-01-31 12:13:36 -0500

    change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
   <div className="App">
     <div className="container">
-      <h1>Weather App</h1>
+   
+      <Weather />
       <footer>
         This project was coded by Stephanie Bui and is {""}
         <a href="https://github.com/stephbui/weather-react-app" target="_blank"> 
