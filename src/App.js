ef72a047a300f6a6cdc49935568441d92e4afc2f
stephbui <stephanie.bui@umontreal.ca>
2021-02-08 22:30:12 -0500

    engine serach

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React from "react";
 import './App.css';
 import Weather from "./Weather";
 
+
 export default function App() {
   return (
-  <div className="App">
-    <div className="container">
-      <Weather defaultCity="Montreal"/>
-      <footer>
-        This project was coded by Stephanie Bui and is {""}
-        <a href="https://github.com/stephbui/weather-react-app" target="_blank"> 
-          open-sourced on GitHub 
-        </a>  
-      </footer>
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Montreal" />
+        <footer>
+          This project was coded by Stephanie Bui and is {""}
+          <a href="https://github.com/stephbui/weather-react-app" target="_blank">
+            open-sourced on GitHub
+        </a>
+        </footer>
+      </div>
     </div>
-  </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./Weatherinfo";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(props) {
+  const [city, setCity] = useState(props.defaultCity);
   const [weatherData, setWeatherData] = useState({ ready: false });
   function handleResponse(response) {
-    console.log(response.data);
     setWeatherData({
       ready: true,
       temperature: response.data.main.temp,
@@ -20,10 +20,25 @@ export default function Weather(props) {
     });
   }
 
+  function search() {
+    const apiKey = "11d5d01e525b3279fea03ff31fdfc61c";
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
@@ -31,6 +46,7 @@ export default function Weather(props) {
                 placeholder="Enter a city..."
                 className="form-control"
                 autoFocus="on"
+                onChange={handleCityChange}
               />
             </div>
             <div className="col-3">
@@ -38,44 +54,12 @@ export default function Weather(props) {
             </div>
           </div>
         </form>
-        <h1> {weatherData.city}</h1>
-        <ul>
-          <li>
-            <FormattedDate date={weatherData.date} />
-          </li>
-          <li className="text-capitalize">{weatherData.description}</li>
-        </ul>
-        <div className="row mt-4">
-          <div className="col-6">
-            <div className="clearfix">
-              <img
-                src={weatherData.iconUrl}
-                alt={weatherData.description}
-                className="float-left"
-              />
-              <div className="float-left">
-                <span className="temperature">
-                  {Math.round(weatherData.temperature)}</span>
-                <span className="unit">°C</span>
-              </div>
-            </div>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Humidity:{weatherData.humidity}%</li>
-              <li>Wind: {weatherData.wind} km/h</li>
-              <li>Feels like: {Math.round(weatherData.feels)} </li>
-            </ul>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData} />
       </div>
     );
 
   } else {
-    const apiKey = "11d5d01e525b3279fea03ff31fdfc61c";
-    let city = "New York";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
     return "Loading..."
   }
 }
