7fc60bb8d770cc2331963658a0b0fcb3aefd00a1
stephbui <stephanie.bui@umontreal.ca>
2021-03-12 10:10:25 -0500

    uptaded

--- src/Weather.js
+++ src/Weather.js
@@ -31,7 +31,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    const apiKey = "a68b01f6acebcedd5ae9fe5ee2fca1da";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
