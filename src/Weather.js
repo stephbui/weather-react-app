739a4b0eb9f441144155b084a769884b9f4dab50
stephbui <stephanie.bui@umontreal.ca>
2021-03-12 10:43:03 -0500

    change name

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import WeatherInfo from "./Weatherinfo";
+import WeatherInfo from "./WeatherInfo";
 import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
